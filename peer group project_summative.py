c1ecfcac54e0eb715c1cf4924854be34bc38f995
ADELINE TUYIZERE <a.tuyizere@alustudent.com>
2021-12-17 23:27:15 +0200

    our codes for the undirected graph implementation

--- peer group project_summative.py
+++ peer group project_summative.py
@@ -19,7 +19,7 @@ class Graph:
     vertices = {}
     edges = []
     edge_indices = {}
-
+# Add a vertex to the set of vertices, and the graph
     def add_vertex(self, vertex):
         if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
             self.vertices[vertex.name] = vertex
@@ -30,7 +30,8 @@ class Graph:
             return True
         else:
             return False
-
+# Add an edge between vertex v1 and v2 with edge weight
+# and check if vertex v1 is a valid vertex
     def add_edge(self, u, v, weight=1):
         if u in self.vertices and v in self.vertices:
             self.edges[self.edge_indices[u]][self.edge_indices[v]] = weight
@@ -55,52 +56,13 @@ g.add_vertex(Vertex('B'))
 for i in range(ord('A'), ord('K')):
     g.add_vertex(Vertex(chr(i)))
 
+#an undirected graph, an edge between v1 v2 does not
+# imply that an edge exists between v2 and v1
+#then implement our graph
+
 edges = ['AB', 'AC', 'BC', 'BF', 'DG', 'GF', 'FE', 'GH', 'HJ', 'JI', 'GI', 'HI']
 for edge in edges:
     g.add_edge(edge[:1], edge[1:])
 
 g.print_graph()
 
-
-def generateGraph():
-    # number of nodes
-    n = 10
-    # probability of edge creation
-    p = 0.8
-    g = erdos_renyi_graph(n, p)
-    print(g.nodes)
-    print(g.edges)
-
-    nodes = g.nodes
-    edges = g.edges
-
-    check = []
-    missing_num = False
-
-    for eachnode in nodes:
-        counter = 0
-        for everyedge in edges:
-            if eachnode in everyedge:
-                counter += 1
-
-        if counter == 0:
-            missing_num = True
-
-    if missing_num:
-        print("The generated graph is not implemented")
-    else:
-        print("The generated graph is implementedd")
-        check.append(0)
-
-    lst = []
-    for node in nodes:
-        for edge in edges:
-            if node in edge:
-                lst.append(node)
-rounds = int(input("How many graphs would you like to be generated? "))
-rounds = float(rounds)
-req = 0.0
-connected_count = 0.0
-euler_count = 0.0
-for i in range(int(rounds)):
-    result = generateGraph()
